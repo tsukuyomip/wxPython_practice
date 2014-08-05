6789283e6411a7ac3f4b367849ae1dc9a72def9a
tsukuyomip <ab0312uo@gmail.com>
2014-08-05 13:18:28 +0900

    サイズ，ポジション，ステータスバーのテスト！

--- practice/hello_wxpython.py
+++ practice/hello_wxpython.py
@@ -5,7 +5,11 @@ import wx
 if __name__ == "__main__":
     application = wx.App()
 
-    frame = wx.Frame(None, wx.ID_ANY, "テストフレーム！")
+    frame = wx.Frame(None, wx.ID_ANY, "テストフレーム！",\
+                     size = (400, 500), pos = (0, 30))
+    frame.CreateStatusBar()
+    frame.SetStatusText("status text test!")
+
     frame.Show()
 
     application.MainLoop()
---
+++ practice/hello_wxpython.py~
@@ -0,0 +1,11 @@
+#coding: utf-8
+
+import wx
+
+if __name__ == "__main__":
+    application = wx.App()
+
+    frame = wx.Frame(None, wx.ID_ANY, "テストフレーム！")
+    frame.Show()
+
+    application.MainLoop()
