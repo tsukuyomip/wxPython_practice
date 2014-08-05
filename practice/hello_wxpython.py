503bb94dc9cc8b3e1b6341f7e176f65458e4a9e0
tsukuyomip <ab0312uo@gmail.com>
2014-08-05 13:26:21 +0900

    パネルの追加

--- practice/hello_wxpython.py
+++ practice/hello_wxpython.py
@@ -7,6 +7,16 @@ if __name__ == "__main__":
 
     frame = wx.Frame(None, wx.ID_ANY, "テストフレーム！",\
                      size = (400, 500), pos = (0, 30))
+    frame.SetBackgroundColour("#AAAAAA")
+
+    r_panel = wx.Panel(frame, wx.ID_ANY, pos = (0, 0), size = (80, 500))
+    r_panel.SetBackgroundColour("#FFAAAA")
+    g_panel = wx.Panel(frame, wx.ID_ANY, pos = (80, 0), size = (80, 500))
+    g_panel.SetBackgroundColour("#AAFFAA")
+    b_panel = wx.Panel(frame, wx.ID_ANY, pos = (160, 0), size = (80, 500))
+    b_panel.SetBackgroundColour("#AAAAFF")
+
+
     frame.CreateStatusBar()
     frame.SetStatusText("status text test!")
 
